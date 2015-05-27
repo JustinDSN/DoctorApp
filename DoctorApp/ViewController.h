12a1649dc9699b4dd2c1c5b2b0e70ce7f76a97af
Justin Steffen <JustinDSN@gmail.com>
2015-05-27 16:50:52 -0700

    Initial Commit

---
+++ DoctorApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DoctorApp
+//
+//  Created by Justin Steffen on 5/27/15.
+//  Copyright (c) 2015 jsteffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DoctorApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DoctorApp
+//
+//  Created by Justin Steffen on 5/27/15.
+//  Copyright (c) 2015 jsteffen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
