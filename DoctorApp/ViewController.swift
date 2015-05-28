ae3180e4b57b34f000df6cca00ccd090631e1ee7
jason <jasonazghani@me.com>
2015-05-27 17:04:19 -0700

    Creating Swift Project

---
+++ DoctorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DoctorApp
+//
+//  Created by jasonazghani on 5/27/15.
+//  Copyright (c) 2015 DoctorApp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
